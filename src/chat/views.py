5e4bff71e3ecf4d9ac7b2c86339e6e0f0b4bcda8
farzana <farzana09bd@gmail.com>
2021-07-17 09:28:16 +0600

    Styling and Bootstrap Ui add

--- src/chat/views.py
+++ src/chat/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
@@ -5,7 +6,9 @@ def index(request):
     return render(request, 'chat/index.html')
 
 
+@login_required
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name_json': room_name,
+        'username': request.user.username,
     })
--- src/justChat/settings.py
+++ src/justChat/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
